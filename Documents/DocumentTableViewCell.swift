002dab2c3063914d135e8c56f48e5db409cdea9d
Joe Timlin <jhthx9@mail.missouri.edu>
2018-07-22 14:58:24 -0500

    Finished core data implementation

--- Documents/DocumentTableViewCell.swift
+++ Documents/DocumentTableViewCell.swift
@@ -12,17 +12,14 @@ class DocumentTableViewCell: UITableViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var sizeLabel: UILabel!
-    @IBOutlet weak var modificationDateLabel: UILabel!
+    @IBOutlet weak var modifiedLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
     }
     
 }
